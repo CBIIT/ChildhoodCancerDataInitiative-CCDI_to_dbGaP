ab2914a45b6f9fb0b66d43c63ec62bd8f16196ec
svburke <42675174+svburke@users.noreply.github.com>
2023-01-12 11:35:56 -0600

    Update CCDI_to_dbGaP.R

--- CCDI_to_dbGaP.R
+++ CCDI_to_dbGaP.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-#Childhood Cancer Data Initiative - CCDI to dbGaP
+#Childrens Cancer Data Initiative - CCDI to dbGaP
 
 #This script will take data from a CCDI submission manifest, and create the Subject Consent, Subject Sample Mapping and Sample Attribute files specifically for a CCDI project.
 
